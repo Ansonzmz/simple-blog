46527854b88292a1d1643ecd908461a10036574d
Anson <zmz211@gmail.com>
2022-03-14 00:34:32 -0700

    form views

--- routes/articles.js
+++ routes/articles.js
@@ -5,4 +5,8 @@ router.get('/new', (req, res) => {
     res.render('articles/new') /* All article contents will be in sub folder */
 })
 
+router.post('/',(req, res) => {
+    
+})
+
 module.exports = router
