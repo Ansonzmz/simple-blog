4cda31eab787736d56556cf4cf84ea18b61cc825
Anson <zmz211@gmail.com>
2022-04-30 00:42:21 -0700

    articles enhancement

--- routes/articles.js
+++ routes/articles.js
@@ -4,21 +4,26 @@ const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.render('articles/new') /* All article contents will be in sub folder */
+    res.render('articles/new', { article: new Article() }) /* All article contents will be in sub folder */
 })
 
-router.get('/:id', (req, res) => {})
+router.get('/:id', async(req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article == null) res.redirect('/')
+    res.render('article/show', { article: article })
+})
 
 router.post('/', async (req, res) => {
-    const article = new Article({
+    let article = new Article({
         title: req.body.title,
-        description: req.body.descriptiontitle,
-        markdown: req.body.marktitle
+        description: req.body.description,
+        markdown: req.body.markdown
     })
     try {
         article = await article.save()
         res.redirect(`/articles/${article.id}`)
     } catch (e) {
+        console.log(e)
         res.render('articles/new', {article: article})
     }
 
